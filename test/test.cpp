e376646c025828354e644f440d416077f73956c5
Daniel <60714674+derodz@users.noreply.github.com>
2025-07-01 01:42:03 +0000

    update test file

--- test/test.cpp
+++ test/test.cpp
@@ -12,10 +12,10 @@ TEST_CASE("Example Test Name - Change me!", "[flag]"){
 	int one = 1;
 
 	// anything that evaluates to false in a REQUIRE block will result in a failing test 
-	REQUIRE(one == 0); // fix me!
+	REQUIRE(one == 1); // fix me!
 
 	// all REQUIRE blocks must evaluate to true for the whole test to pass
-	REQUIRE(false); // also fix me!
+	REQUIRE(true); // also fix me!
 }
 
 TEST_CASE("Test 2", "[flag]"){
@@ -33,21 +33,4 @@ TEST_CASE("Test 2", "[flag]"){
 	};
 
 	// each section runs the setup code independently to ensure that they don't affect each other
-}
-
-// you must write 5 unique, meaningful tests for credit on the testing portion of this project!
-
-// the provided test from the template is below.
-
-TEST_CASE("Example BST Insert", "[flag]"){
-	/*
-		MyAVLTree tree;   // Create a Tree object
-		tree.insert(3);
-		tree.insert(2);
-		tree.insert(1);
-		std::vector<int> actualOutput = tree.inorder();
-		std::vector<int> expectedOutput = {1, 2, 3};
-		REQUIRE(expectedOutput.size() == actualOutput.size());
-		REQUIRE(actualOutput == expectedOutput);
-	*/
 }
