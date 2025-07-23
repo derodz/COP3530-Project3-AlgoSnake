e3ffae3fa7e58d789e0328f01e28fa04090002b7
IanRPage <ipragmatic2r@gmail.com>
2025-07-23 10:50:01 -0400

    created templated Graph structure
    i created a base for our Graph structure. using an adjacency matrix for
    representation since graph is dense.
    
    also altered CMakeLists.txt a little bit, along with directory structure

---
+++ include/Graph.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <sstream>
+#include <stdexcept>
+#include <utility>
+#include <vector>
+
+// stored {up, down, left, right}
+const std::pair<int, int> STEPS[4] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+template <typename T> class Graph {
+  int rows = 0, cols = 0;
+  std::vector<std::vector<T>> adjMatrix;
+
+public:
+  Graph();
+  Graph(int r, int c) : rows(r), cols(c), adjMatrix(r, std::vector<T>(c)) {};
+
+  void setMatrixDims(int r, int c) {
+    rows = r;
+    cols = c;
+    adjMatrix.resize(r);
+    for (auto &row : adjMatrix) {
+      row.resize(c);
+    }
+  };
+
+  int getRows() const { return rows; };
+  int getCols() const { return cols; };
+
+  T &getMatrixNode(int y, int x) {
+    if (y < 0 || x < 0 || y >= rows || x >= cols) {
+      std::stringstream ss;
+      ss << "Coordinate {" << y << ", " << x << "} is outside of matrix";
+      throw std::out_of_range(ss.str());
+    }
+    return adjMatrix[y][x];
+  }
+  const T &getMatrixNode(int y, int x) const {
+    if (y < 0 || x < 0 || y >= rows || x >= cols) {
+      std::stringstream ss;
+      ss << "Coordinate {" << y << ", " << x << "} is outside of matrix";
+      throw std::out_of_range(ss.str());
+    }
+    return adjMatrix[y][x];
+  }
+
+  std::vector<std::pair<int, int>> getNodeNeighbors(int y, int x) {
+    std::vector<std::pair<int, int>> neighbors;
+		for (const auto &step : STEPS) {
+			int dy = step.first, dx = step.second;
+			int ny = y + dy, nx = x + dx;
+			if (ny >= 0 && nx >= 0 && ny < rows && nx < cols) {
+				neighbors.push_back({ny, nx});
+			}
+		}
+		return neighbors;
+  }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
+#include <Graph.h>
 #include <iostream>
 
 using namespace std;
 
-int main(){
-	cout << "Hello AlgoSnake!\n";
-	return 0;
+int main() {
+	Graph<int> testing(10, 10);
+
+	std::vector<std::pair<int, int>> neighbors = testing.getNodeNeighbors(4, 4);
+	std::cout << "Neighbors of (4, 4):\n";
+	for (const auto &pair : neighbors) {
+		std::cout << "(" << pair.first << ", " << pair.second << ")\n";
+	}
+
+	neighbors = testing.getNodeNeighbors(0, 0);
+	std::cout << "Neighbors of (0, 0):\n";
+	for (const auto &pair : neighbors) {
+		std::cout << "(" << pair.first << ", " << pair.second << ")\n";
+	}
 }
