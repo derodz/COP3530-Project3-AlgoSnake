45470cff654d6947e17a1b72be25cc00f565dbe0
Daniel Rodriguez <daniel@mynoteable.com>
2025-08-03 00:31:32 -0400

    move until death or path available

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -72,9 +72,35 @@ void Game::update()
     }
     if (path.empty())
     {
-      dead = true;
-      cout << "Game ended after " << elapsedTime << " seconds.";
-      return;
+      // no current path found, try continuing in the current direction
+      auto startOfSnakeBody = next(snake.begin());
+      int right = head.second + 1;
+      int up = head.first + 1;
+      int left = head.second - 1;
+      int down = head.first - 1;
+      if (right < grid.getCols() - 1 && right != (*startOfSnakeBody).second)
+      {
+        path.push_back({head.first, right});
+      }
+      else if (left >= 0 && left != (*startOfSnakeBody).second)
+      {
+        path.push_back({head.first, left});
+      }
+      else if (up < grid.getRows() - 1 && up != (*startOfSnakeBody).first)
+      {
+        path.push_back({up, head.second});
+      }
+      else if (down >= 0 && down != (*startOfSnakeBody).first)
+      {
+        path.push_back({down, head.second});
+      }
+      else
+      {
+        // no valid moves left, game over
+        dead = true;
+        cout << "Game ended after " << elapsedTime << " seconds.";
+        return;
+      }
     }
   }
   pair<int, int> newHead = path.front();
@@ -85,7 +111,9 @@ void Game::update()
   if (newHead.first < 0 || newHead.first >= rows || newHead.second < 0 ||
       newHead.second >= cols)
   {
+    // hit wall
     dead = true;
+    cout << "Game ended after " << elapsedTime << " seconds.";
     return;
   }
 
@@ -111,6 +139,7 @@ void Game::update()
     if (*it == head)
     {
       dead = true;
+      cout << "Game ended after " << elapsedTime << " seconds.";
       break;
     }
   }
